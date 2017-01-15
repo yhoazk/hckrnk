b121d6616700bde0fe2b5327e2413cf9b1e71fe3
Gustavo Espindola <yhoazk@gmail.com>
2017-01-15 14:20:24 -0600

    lined list

---
+++ linked_lists.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <cstddef>
+using namespace std;
+class Node
+{
+    public:
+        int data;
+        Node *next;
+        Node(int d){
+            data=d;
+            next=NULL;
+        }
+};
+class Solution{
+//----------
+    public:
+      Node* insert(Node *head,int data)
+      {
+        //Complete this method
+        Node *new_node = new Node(data);
+        Node* head_bkp = head;
+        if(head == NULL)
+        {
+          //first element
+          head_bkp = new_node;
+        }
+        else
+        {
+          while (head->next != NULL) {
+              head = head->next;
+          }
+          head->next = new_node;
+        }
+
+
+        return head_bkp;
+      }
+//----------
+      void display(Node *head)
+      {
+        Node *start=head;
+        while(start)
+        {
+          cout<<start->data<<" ";
+          start=start->next;
+        }
+      }
+};
+int main()
+{
+Node* head=NULL;
+Solution mylist;
+int T,data;
+cin>>T;
+while(T-->0){
+  cin>>data;
+  head=mylist.insert(head,data);
+}
+mylist.display(head);
+
+}
