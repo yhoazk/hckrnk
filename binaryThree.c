abdf94a869bfdd6cf06101680b7fbc82a65e675c
Gustavo Espindola <yhoazk@gmail.com>
2017-01-15 14:21:44 -0600

    day23 hacker rank Binary three

---
+++ binaryThree.c
@@ -0,0 +1,60 @@
+#include <math.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+/*
+
+*/
+
+
+typedef struct Node{
+    struct Node* left;
+    struct Node* right;
+    int data;
+}Node;
+Node* newNode(int data){
+    Node* node=(Node*)malloc(sizeof(Node));
+    node->left=node->right=NULL;
+    node->data=data;
+    return node;
+}
+
+
+void levelOrder(Node* root){
+  if(root == NULL){
+      return;
+  }
+  printf("%d ", root->data);
+  levelOrder(root->left);
+  levelOrder(root->right);
+}
+
+Node* insert(Node* root,int data){
+    if(root==NULL)
+        return newNode(data);
+    else{
+        Node* cur;
+        if(data<=root->data){
+            cur=insert(root->left,data);
+            root->left=cur;
+        }
+        else{
+            cur=insert(root->right,data);
+            root->right=cur;
+        }
+
+    }
+    return root;
+}
+int main(){
+    Node* root=NULL;
+    int T,data;
+    scanf("%d",&T);
+    while(T-->0){
+        scanf("%d",&data);
+        root=insert(root,data);
+    }
+    levelOrder(root);
+    return 0;
+
+}
