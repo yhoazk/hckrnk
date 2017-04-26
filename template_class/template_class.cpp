0d03b2f6c7275e5efa3c150a13a81ed602d24a84
Gustavo Espindola <yhoazk@gmail.com>
2017-04-26 12:35:49 -0500

    solution c class templates
    https://www.hackerrank.com/challenges/c-class-templates

---
+++ template_class/template_class.cpp
@@ -0,0 +1,68 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+#include <cassert>
+using namespace std;
+
+
+/*********************************************************************************/
+    /* Solution */ 
+/*Write the class AddElements here*/
+template<class T>
+class AddElements{
+    T element_a;
+    public:
+    AddElements(T init){
+        element_a = init;
+    } 
+    T add(T x)
+    {
+        return x + element_a;
+    }
+};
+
+
+template<>
+class AddElements<string>{
+    string element_a;
+    public:
+    AddElements(string init){
+        element_a = init;
+    } 
+    string concatenate(string x)
+    {
+        return  element_a + x;
+    }
+};
+
+/*********************************************************************************/
+int main ()
+{
+  int n,i;
+  cin >> n;
+  for(i=0;i<n;i++) {
+    string type;
+    cin >> type;
+    if(type=="float") {
+        double element1,element2;
+        cin >> element1 >> element2;
+        AddElements<double> myfloat (element1);
+        cout << myfloat.add(element2) << endl;
+    }
+    else if(type == "int") {
+        int element1, element2;
+        cin >> element1 >> element2;
+        AddElements<int> myint (element1);
+        cout << myint.add(element2) << endl;
+    }
+    else if(type == "string") {
+        string element1, element2;
+        cin >> element1 >> element2;
+        AddElements<string> mystring (element1);
+        cout << mystring.concatenate(element2) << endl;
+    }
+  }
+  return 0;
+}
