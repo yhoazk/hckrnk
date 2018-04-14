e643e4fe59344c0d2df614788715fccac1c4420c
yhoazk <yhoazk@gmail.com>
2018-04-14 10:40:14 +0200

    Replaced function with lambdas

--- repeadred_string/repeated_string.cpp
+++ repeadred_string/repeated_string.cpp
@@ -6,16 +6,18 @@ using namespace std;
  * Function to find the number of characters c in the 
  * string s for the range [start, end]
 */
-long num_c_in_s(char c, string& s, long start, long end)
-{
-  return std::count(s.begin(), s.begin()+end, c);
-}
-
+//long num_c_in_s(char c, string& s, long start, long end)
+//{
+//  return std::count(s.begin(), s.begin()+end, c);
+//}
+// [](){}
 long repeatedString(string s, long n) {
-    long ains = num_c_in_s('a', s, 0, s.length());
-    long  full = n/s.length();
-    long part = n % s.length();
-    return (full * ains) + num_c_in_s('a',s,0,part);
+    //long ains = num_c_in_s('a', s, 0, s.length());
+    auto d = [](char c,string s, long n){ return count(s.begin(), s.begin()+n, c);};
+//    long ains = d('a', s, s.length());
+//    long  full = n/s.length();
+//    long part = n % s.length();
+    return (n/s.length() * d('a', s, s.length())) + d('a',s,n%s.length());
 }
 
 int main() {
