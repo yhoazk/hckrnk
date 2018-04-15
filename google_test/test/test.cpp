de0bfb7703c2b27585b1c8e9152b4e8bb8db00ed
yhoazk <yhoazk@gmail.com>
2018-04-15 16:04:47 +0200

    Working gtest

--- google_test/test/test.cpp
+++ google_test/test/test.cpp
@@ -1,7 +1,13 @@
+#if 0
 #include <iostream>
-#include "gtest/gtest.h"
+#include <gtest/gtest.h>
 
-TEST(SquareRootTest, PositiveNos) 
+double square_root(double x)
+{
+  return x/2;
+}
+
+TEST(SquareRootTest, PositiveNos)
 {
     EXPECT_EQ (18.0, square_root (324.0));
     EXPECT_EQ (25.4, square_root (645.16));
@@ -18,4 +24,32 @@ int main(int argc, char **argv)
 {
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
+}
+#endif
+
+#include <iostream>
+#include "gtest/gtest.h"
+
+class TestFixture : public ::testing::Test {
+protected:
+    void SetUp(){
+	std::cout << "SetUp()" << std::endl;
+    }
+
+    void TearDown(){
+	std::cout << "TearDown()" << std::endl;
+    }
+};
+
+
+
+TEST_F (TestFixture, shouldCompile) {
+    std::cout << "shouldCompile" << std::endl;
+    ASSERT_TRUE(true); // works, maybe optimized out?
+
+}
+
+int main(int argc, char **argv) {
+    ::testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
 }
