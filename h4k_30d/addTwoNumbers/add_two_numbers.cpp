86aaa4cee8ea1215096c57f1a1420f342a0c0b87
yhoazk <yhoazk@gmail.com>
2018-10-28 22:11:38 +0100

    sol

--- h4k_30d/addTwoNumbers/add_two_numbers.cpp
+++ h4k_30d/addTwoNumbers/add_two_numbers.cpp
@@ -3,33 +3,6 @@
 // bazel build ... --strategy=CppCompile=standalone
 // bazel build ... --compilation_mode=dbg --strategy=CppCompile=standalone
 
-struct ListNode {
-    int val;
-    ListNode *next;
-    // This is the example where the phase of allocation and
-    // initialization make the things more complicated
-    // There should be a default constructor which only allocates
-    ListNode(int x) : val(x), next(nullptr) {}
-};
-
-//////// GIVEN //////////////////////////////////////
-/**
- * Definition for singly-linked list.
- * struct ListNode {
- *     int val;
- *     ListNode *next;
- *     ListNode(int x) : val(x), next(NULL) {}
- * };
- */
-class Solution {
-public:
-    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
-        
-    }
-};
-
-//////// GIVEN //////////////////////////////////////
-
 /* reverse linked list in place */
 /*  Not really needed, just  nice to have */
 /* Expects the root node, returns the end node and hte root as end node */
@@ -54,13 +27,64 @@ L* reverseList(L *list){
     return(curr);
 }
 
+
+struct ListNode {
+    int val;
+    ListNode *next;
+    // This is the example where the phase of allocation and
+    // initialization make the things more complicated
+    // There should be a default constructor which only allocates
+    ListNode(int x) : val(x), next(nullptr) {}
+};
+
+//////// GIVEN //////////////////////////////////////
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ * can the linked lists be of different size?
+ */
+class Solution {
+public:
+    ListNode* addTwoNumbers(ListNode* l1, ListNode* l2) {
+        using namespace std;
+        ListNode* rl1 = reverseList(l1);    
+        ListNode* rl2 = reverseList(l2);
+        int rem = 0;
+        int res = 0;
+        int sum = 0;
+        bool done;
+        while(true){
+            sum = rl1->val + rl2->val + rem;
+            if(sum >= 10){
+                rem = static_cast<int>(sum / 10);
+                sum = sum % 10;
+            }
+            cout << "val1: " << rl1->val << " val2: " << rl2->val << " : rl2+rl1 = " << sum << '\n';
+            if(rl1->next == nullptr or rl2->next == nullptr){
+                break;
+            }
+            rl1 = rl1->next;
+            rl2 = rl2->next;
+        }
+        return rl1;
+    }
+};
+
+//////// GIVEN //////////////////////////////////////
+
+
+
 /*how to keep the address of the root node */
 // Flaky method
 ListNode* createListNode(const std::string& list){
 
     //ListNode tmp('g');
     ListNode* index;
-    ListNode* root = new ListNode( static_cast<int>( *std::begin(list) ) );
+    ListNode* root = new ListNode( static_cast<int>( *std::begin(list) -0x30) );
     if(1 == list.size())
     {
         return root;
@@ -69,16 +93,16 @@ ListNode* createListNode(const std::string& list){
     for(auto i = std::begin(list)+1; i != std::end(list)-1; ++i){
         std::cout << ":: " << static_cast<char>(*i);
         //root->val = static_cast<int>(c);
-        root->next = new ListNode(static_cast<int>(*i));
+        root->next = new ListNode(static_cast<int>(*i - 0x30));
         if(first){
             first = false;
             index = root;
-            std::cout << "\nf: " << index->val << '\n';
+            std::cout << "\nf: " << static_cast<char>(index->val) << '\n';
         }
         root = root->next;
     }
     // add the last element
-    root->next = new ListNode(static_cast<int>(list.back()));
+    root->next = new ListNode(static_cast<int>(list.back() - 0x30));
     std::cout << std::endl;
     return(index);
 }
@@ -94,7 +118,7 @@ void printListNode(ListNode* l){
     index = const_cast<ListNode*>(l);
 
     while(true){
-        cout << static_cast<char>(index->val);
+        cout << static_cast<char>(index->val + 0x30);
         index = index->next;
         if(index == nullptr){
             break;
@@ -107,9 +131,11 @@ void printListNode(ListNode* l){
 int main(int argc, char const *argv[])
 {
     // TODO: HAndle list of size 2 or 3
-   ListNode* l = createListNode("12456789abcdefghijklmnopqrts");
-   printListNode(l);
-   ListNode* rl = reverseList(l);
-   printListNode(rl);
+   ListNode* l1 = createListNode("243");
+   ListNode* l2 = createListNode("564");
+   printListNode(l1);
+   printListNode(l2);
+   Solution s{};
+   s.addTwoNumbers(l1, l2);
     return 0;
 }
