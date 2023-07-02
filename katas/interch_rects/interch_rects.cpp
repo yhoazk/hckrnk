6eed5773c89002c0daa8e562dd3a8cddc7c412f9
yhoazk <yhoazk@gmail.com>
2023-07-02 20:56:07 +0200

    Katas test passing

---
+++ katas/interch_rects/interch_rects.cpp
@@ -0,0 +1,8 @@
+#include <bits/stdc++.h>
+#include <gtest/gtest.h>
+
+// https://leetcode.com/problems/number-of-pairs-of-interchangeable-rectangles/
+
+TEST(interch_rect, sample1) {
+    ASSERT_TRUE(false);
+}
